7e94c003a34cbfae9597d5e936512b963d389d2a
maximilianoguitian25-create <maximilianoguitian25@gmail.com>
2026-06-05 17:41:15 -0300

    Refactor automata structure and transition functions

--- afd.c
+++ afd.c
@@ -14,7 +14,7 @@ void leecad(char cad[], int tam){
 	if(m !=EOF && m!= '\n')
 		while((m=getchar ()) != EOF && m!='\n');
 }
-
+	
 tdata ing(){
 	char buffer[100];
 	leecad(buffer,100);
@@ -22,46 +22,28 @@ tdata ing(){
 	nuevo->string = load2(buffer);
 	return nuevo;
 }
-
-automata crea_uno(tdata conj_q, tdata alfa_sig, tdata ini, tdata conj_f){
+		
+automata crea_uno(tdata conj_q, tdata alfa_sig, int ini, tdata conj_f){
 	
-	automata A =(automata)malloc(sizeof(struct automataNode));
+	automata A =(automata)malloc(sizeof(Automata));
 	if( A ==NULL)
 		return NULL;
 	
-	A->q = conj_q;
-	A->sig = alfa_sig;
+	A->Q = conj_q;
+	A->Sigma = alfa_sig;
 	A->q0 = ini;
-	A->f = conj_f;
+	A->F = conj_f;
 	
-	A->cantest = length(conj_q);
-	A->cantsim = length(alfa_sig);
-	
-	A->tabla = (tdata**)malloc(A->cantest* sizeof(tdata*));
-	if(A->tabla ==NULL){
-		free(A);
-		return NULL;
-		
-	}
-	int i;
-	for( i=0;i<A->cantest; i++){
-		
-		A->tabla[i] =(tdata*) malloc (A->cantsim * sizeof(tdata));
-		
-		int j;
-		for(j=0;  j<A->cantsim; j++){
-			A->tabla[i][j] = NULL;
-		}
-	}
+	A->delta = NULL;
+	A->deltaSize = 0;
 	
 	return A;
 }
-	
-
+			
 int indice_esta(automata a, tdata estado){
-	if( a == NULL || a->q == NULL || estado == NULL)
+	if( a == NULL || a->Q == NULL || estado == NULL)
 		return -1;
-	tdata aux = a->q;
+	tdata aux = a->Q;
 	int i= 0;
 	while(aux!=NULL){
 		
@@ -73,13 +55,13 @@ int indice_esta(automata a, tdata estado){
 	}
 	return -1;
 }
-	
-
+				
+				
 int indice_simb( automata a, tdata simbolo){
 	
-	if(a == NULL || a->sig == NULL || simbolo == NULL) 
+	if(a == NULL || a->Sigma == NULL || simbolo == NULL)
 		return -1;
-	tdata aux = a->sig;
+	tdata aux = a->Sigma;
 	int i=0;
 	while( aux!= NULL){
 		if( aux->data != NULL && igual(aux->data, simbolo)){
@@ -93,21 +75,36 @@ int indice_simb( automata a, tdata simbolo){
 	return -1;
 	
 }
-	
 
-void agregar_transi(automata A, tdata ini, tdata simbolo , tdata est_dest){
+
+void agregar_transi(automata A,int from, int symbol, tdata est_dest){
 	
-	if(A==NULL || ini == NULL || simbolo == NULL || est_dest == NULL)
+	if(A==NULL || est_dest == NULL)
 		return;
-	int f = indice_esta( A, ini);
-	int c = indice_simb(A, simbolo);
-		
-	if( f == -1 || c == -1){
-		printf("ERROR");
-		return;
+	int i;
+	for(i=0;i<A->deltaSize;i++){//puedo utilizar el return o tengo que mandarle una bandera
+		if(A->delta[i].from == from && A->delta[i].symbol == symbol){
+			
+			tdata copia_dest = clone(est_dest);
+			tdata aux = copia_dest;
+			while(aux != NULL){
+				if(aux->data != NULL)
+					insert_set(&(A->delta[i].to),aux->data);
+				aux=aux->next;
+			}
+			free_tdata(copia_dest);
+			return;
+		}
 	}
 	
-	insert_set(&( A->tabla[f][c]), est_dest);
+	A->deltaSize++;
+	A->delta = (TransitionEntry*)malloc(A->delta, A->deltaSize* sizeof(TransitionEntry));
+	
+	A->delta[A->deltaSize-1].from = from;
+	A->delta[A->deltaSize - 1].symbol = symbol;
+	A->delta[A->deltaSize - 1].to = clone(est_dest);
+	
+	
 }
 
 void mostrar(automata a){
@@ -116,40 +113,44 @@ void mostrar(automata a){
 	printf("\n TABLA DE TRANSICIONES\n");
 	
 	printf("%-15s","Estado");
-	tdata aux = a->sig;
+	tdata aux = a->Sigma;
 	while(aux != NULL){
 		if(aux->data != NULL){
 			printf("| ");
 			print_tdata(aux->data);
-			printf("    ");
+			printf("    ");
 		}
 		aux = aux->next;
 	}
 	printf("\n--------------------\n");
-	tdata b = a->q;
+	tdata b = a->Q;
 	int i = 0;
 	while( b != NULL){
 		if( b->data != NULL){
 			print_tdata(b->data);
 			printf(" ");
 			
-			int j;
-			for(j=0; j< a->cantsim;j++){
+			tdata aux_sim = a->Sigma;
+			int j=0;
+			while(aux_sim != NULL){
 				printf("| ");
-				if(a->tabla[i][j] ==  NULL){
-					printf("{}   ");
-					
-				}else{
-					tdata d= a->tabla[i][j];
-					printf("{");
-					while(d != NULL){
-						print_tdata(d->data);
-						if(d->next != NULL)
-							printf(",");
-						d=d->next;
+				tdata cel_dest = NULL;
+				int k=0, ban=0;
+				while(k< a->deltaSize && b==0){
+					if(a->delta[k].from == i&& a->delta[k].symbol == j){
+						cel_dest = a->delta[k].to;
+						ban=1;
 					}
-					printf("}");
+					k++;
 				}
+				if(cel_dest == NULL)
+					  printf("{}   ");
+				else {
+						  print_tdata(cel_dest);
+						  printf("   ");
+				}
+			  j++;
+			  aux_sim =aux_sim->next;
 			}
 			printf("\n");
 			
@@ -161,129 +162,140 @@ void mostrar(automata a){
 	
 }
 int es_afd(automata a){
-	if(a==NULL || a->tabla == NULL )
+	if(a==NULL)
 		return 0;
-	int i,j;
-	for(i=0; i< a->cantest; i++){
+	int i;
+	for(i=0; i< a->deltaSize; i++){
 		
-		for(j = 0; j< a->cantsim;j++){
-			if(a->tabla[i][j] != NULL){
-				if(length(a->tabla[i][j]) > 1)
-					return 0;
-			}
+		if(a->delta[i].to != NULL){
+			if(length(a->delta[i].to) > 1)
+				return 0;
+			
 		}
 	}
 	return 1;
 }
 
 void liberar(automata a){
-	int i, j;
+	
 	if(a == NULL)
 		return;
-	if(a->tabla != NULL){
-		for(i=0;i<a->cantest; i++){
-			for(j=0;j< a->cantsim; j++){
-				if(a->tabla[i][j] != NULL)
-					free_tdata(a->tabla[i][j]);
-			}
-			free(a->tabla[i]);
+	if(a->delta != NULL){
+		int i;
+		for(i=0;i<a->deltaSize; i++){
+			if(a->delta[i].to != NULL)
+				free_tdata(a->delta[i].to);
 		}
-		free(a->tabla);
+		free(a->delta);
 	}
+	if(a->Q != NULL)
+	      free_tdata(a->Q);
+	if(a->Sigma != NULL)
+		free_tdata(a->Sigma);
+	if(a->F != NULL)
+		free_tdata(a->F);
 	free(a);
 }
-
 automata carga_aut(){
-	tdata conj_q = NULL, alf = NULL, est_fin = NULL, ini = NULL;
+	tdata conj_q = NULL, alf = NULL, est_fin = NULL;
 	int n, i;
 	
 	printf("INICIO");
-	printf("¿Cuantos estados son?");
+	printf("¿Cuantos estados son?\n");
 	scanf("%d",&n);
+	getchar();
 	
 	for(i=0;i<n; i++){
-		printf("estado %d:",i);
+		printf("estado %d:\n",i);
 		tdata e=ing();
 		insert_set(&conj_q,e);
-		if( i == 0 )
-			ini = e;
+		free_tdata(e);
 	}
 	
-	printf("¿Cuantos simbolos?");
+	printf("¿Cuantos simbolos?\n");
 	scanf("%d",&n);
+	getchar();
 	
 	for(i=0;i<n; i++){
-		printf("simbolo %d:",i);
+		printf("simbolo %d:\n",i);
 		tdata c= ing();
 		insert_set(&alf,c);
+		free_tdata(c);
 		
 	}
 	
-	printf("¿Cuanto estados son finales?");
+	printf("¿Cuanto estados son finales?\n");
 	scanf("%d",&n);
+	getchar();
 	for(i=0;i<n;i++){
-		printf("estado final %d",i);
+		printf("estado final %d\n",i);
 		tdata f = ing();
 		
 		tdata aux = conj_q;
 		int val = 0;
-		while(aux != NULL){
-			if(igual(aux->data, f))
+		while(aux != NULL && val == 0){
+			if(aux->data !=NULL && igual(aux->data, f))
 				val = 1;
 			aux = aux->next;
 			
 		}
 		if(val == 1)
-			insert_set( &est_fin,f);
+			    insert_set(&est_fin,f);
 		else
-		printf("ERROR");
+			printf("ERROR 1\n");
+		free_tdata(f);
 	}
 	
-	automata A= crea_uno(conj_q, alf, ini, est_fin);
+	automata A= crea_uno(conj_q, alf, 0, est_fin);
 	
-	printf("TRANSICIONES");
-	printf("ingrese FIN para salir");
-	int b = 1;
-	while(b ==1){
-		printf("estado origen:");
+	printf("TRANSICIONES\n");
+	printf("ingrese FIN para salir\n");
+	int b = 0;
+	while(b ==0){
+		printf("estado origen:\n");
 		tdata ori = ing();
-		if(ori->string != NULL && strcmp (ori->string,"FIN")==0){
-			b = 0;
+		if(ori->string != NULL && strcmp (ori->string,"FIN\n")==0){
+			b = 1;
 			free_tdata(ori);
 		}else{
-			if(indice_esta(A,ori) != -1){
-				printf("simbolo:");
+			int f = indice_esta(A,ori);
+			if(f !=-1){
+				printf("simbolo:\n");
 				tdata sim = ing();
-				printf("estado destino:");
+				printf("estado destino:\n");
 				tdata dest = ing();
-				
-				if(indice_simb(A,sim) !=-1 && indice_esta(A,dest)!= -1){
-					agregar_transi(A,ori,sim,dest);
+				int c=indice_simb(A,sim);
+				int d = indice_esta(A,dest);
+				if(c!=-1 && d!=-1){
+					tdata des = NULL;
+					insert_set(&des,dest);
+					agregar_transi(A,f,c,des);
 					printf("Transicion Agregada\n");
+					free_tdata(des);
 				}
 				else{
-					printf("Error\n");
-					free_tdata(sim);
-					free_tdata(dest);
+					printf("Error2\n");
 				}
+				free_tdata(sim);
+				free_tdata(dest);
 			}
 			else
-			   printf("ERROR\n");
+				printf("ERROR3\n");
 			free_tdata(ori);
 		}
 	}
 	return A;
 }
-	
+
 int vali_cad(automata a, tdata cad){
 	if(a == NULL || cad == NULL)
 		return 0;
 	tdata c= cad;
 	while( c!= NULL){
 		if(c->data != NULL){
-		
+			
 			if(indice_simb(a, c->data) == -1){
-				printf("no pertenece al alfabeto cargado\n");
+				printf("no pertenece al alfabeto \n");
 				return 0;
 			}
 		}
@@ -291,23 +303,30 @@ int vali_cad(automata a, tdata cad){
 	}
 	return 1;
 }
-
+											
 int pertene_cadena( automata a, tdata cad_list){
-	if(a==NULL || a->tabla == NULL || cad_list == NULL)
+	if(a==NULL || cad_list == NULL)
 		return 0;
 	if(cad_list ->nodeType != LIST){
-		printf("debe ser una cadena");
+		printf("debe ser una lista\n");
 		return 0;
 	}
 	
 	if(!vali_cad(a,cad_list))
-	   return 0;
+	      return 0;
 	
 	
 	tdata est_act = NULL;
-	insert_set(&est_act, a->q0);
+	tdata aux = a->Q;
+	int i =0;
 	
-	tdata c = cad_list;
+	while(aux !=NULL && i != a->q0){
+		i++;
+		aux = aux->next;
+	}
+	if(aux != NULL)
+		  insert_set(&est_act, aux->data);
+	tdata c =cad_list;
 	while(c!= NULL){
 		tdata sim= c->data;
 		
@@ -321,12 +340,18 @@ int pertene_cadena( automata a, tdata cad_list){
 				tdata act = aux_act->data;
 				int f = indice_esta(a,act);
 				if(f!=-1){
-					tdata dest = a->tabla[f][col];
-					if( dest != NULL){
-						tdata cop_uni = union_set(prox_est, dest);
-						if(prox_est != NULL)
-							free_tdata(prox_est);
-						prox_est = cop_uni;
+					int k=0;
+					int enc = 0;
+					while(k< a->deltaSize && enc ==0){
+						if(a->delta[k].from == f && a->delta[k].symbol == col){
+							tdata cop_uni = union_set(prox_est, a->delta[k].to);
+							if(prox_est != NULL)
+								free_tdata(prox_est);
+							prox_est = cop_uni;
+							enc = 1;
+						}
+						k++;
+						
 					}
 				}
 				aux_act = aux_act->next;
@@ -340,7 +365,7 @@ int pertene_cadena( automata a, tdata cad_list){
 		}
 		c=c->next;
 	}
-	tdata inter = intersection_set(est_act, a->f);
+	tdata inter = intersection_set(est_act, a->F);
 	int acep = (length(inter)>0);
 	
 	free_tdata(inter);
@@ -350,37 +375,11 @@ int pertene_cadena( automata a, tdata cad_list){
 }
 tdata renom(int i){
 	char buffer[20];
-	sprintf(buffer, "p%d", i); // Formatea directamente "p0", "p1", etc.
-	
-	tdata nuevo = create_str_ast();
+	sprintf(buffer, "p%d", i); 
+		tdata nuevo = create_str_ast();
 	nuevo->string = load2(buffer);
 	return nuevo;
 }	
-	/*
-tdata renom(int i){
-	char buffer[20];
-	buffer[0]= 'p';
-	if(i==0){
-		buffer[1] = '0';
-		buffer[2] = '\0':
-		
-	}else{
-		char inv[15];
-		int p_inv = 0;
-		int temp = i;
-		
-		while(temp >0){
-			inv[p_inv] = (temp %10) + '0';
-			p_inv++;
-			temp = temp/10;
-		}
-		int p_bu
-	}
-	tdata nuevo = create_str_ast();
-	nuevo->string = load2(buffer);
-	return nuevo;
-}
-*/
 tdata bus_nom(tdata lis_sub, tdata lis_nom, tdata sub_bus){
 	tdata aux_sub = lis_sub;
 	tdata aux_nom = lis_nom;
@@ -408,21 +407,27 @@ automata convertir(automata afnd){
 	}
 	
 	int cont_p = 0;
-	tdata nue_q = NULL, nue_f = NULL, ini_n = NULL;
+	tdata nue_q = NULL, nue_f = NULL;
 	
 	tdata conj_i = NULL;
-	insert_set(&conj_i, afnd->q0);
+	tdata aux = afnd->Q;
+	int i =0;
+	while(aux != NULL && i != afnd->q0){
+		i++;
+		aux = aux->next;
+	}
+	if(aux != NULL)
+		  insert_set(&conj_i, aux->data);
 	
-	ini_n = renom(cont_p++);
+	tdata ini_n = renom(cont_p++);
 	insert_set(&nue_q, ini_n);
 	
-	tdata l= intersection_set(conj_i, afnd->f);
+	tdata l= intersection_set(conj_i, afnd->F);
 	if(length(l)>0)
 		insert_set(&nue_f,ini_n);
 	free_tdata(l);
 	
-	tdata sub = NULL;
-	tdata nom = NULL;
+	tdata sub = NULL, nom = NULL;
 	append(&sub, conj_i);
 	append(&nom, ini_n);
 	
@@ -431,14 +436,15 @@ automata convertir(automata afnd){
 	append(&sub_p, conj_i);
 	append(&nom_p, ini_n);
 	
-	automata afd_res = crea_uno(nue_q, afnd->sig, ini_n, nue_f);
+	automata afd_res = crea_uno(nue_q, afnd->Sigma, 0, nue_f);
 	
 	while(sub_p != NULL){
 		
 		tdata sub_a = sub_p->data;
 		tdata nom_a = nom_p->data;
 		
-		tdata aux_sim = afnd->sig;
+		tdata aux_sim = afnd->Sigma;
+		int le=0;
 		while (aux_sim != NULL){
 			tdata sim = aux_sim->data;
 			tdata conj_t = NULL;
@@ -446,13 +452,19 @@ automata convertir(automata afnd){
 			tdata aux_sub = sub_a;
 			while(aux_sub != NULL){
 				int f = indice_esta(afnd, aux_sub->data);
-				int c = indice_simb(afnd, sim);
-				if ( f != -1 && c!= -1 && afnd->tabla[f][c] != NULL){
-					tdata u = union_set(conj_t, afnd->tabla[f][c]);
-					if(conj_t != NULL)
-						free_tdata(conj_t);
-					conj_t= u;
-					
+				if(f!= -1){
+					int k=0;
+					int enc =0;
+					while(k<afnd->deltaSize && enc ==0){
+						if(afnd->delta[k].from == f && afnd->delta[k].symbol == le){
+							tdata u = union_set(conj_t, afnd->delta[k].to);
+							if(conj_t != NULL)
+								free_tdata(conj_t);
+							conj_t= u;
+							enc =1;
+						}
+						k++;
+					}
 				}
 				aux_sub = aux_sub->next;
 			}
@@ -462,16 +474,10 @@ automata convertir(automata afnd){
 				if(nom_d == NULL){
 					nom_d = renom(cont_p ++);
 					
-					insert_set(&(afd_res->q), nom_d);
-					afd_res->cantest = length(afd_res->q);
-					afd_res->tabla = (tdata**)realloc(afd_res->tabla, afd_res->cantest * sizeof(tdata*));
-					afd_res->tabla[afd_res->cantest -1] = (tdata*)malloc(afd_res->cantsim * sizeof(tdata));
-					int k;
-					for(k=0; k< afd_res->cantsim; k++)
-						afd_res->tabla[afd_res->cantest -1][k] = NULL;
-					tdata inter_f = intersection_set(conj_t, afnd->f);
-					if(length(inter_f) >0)
-						insert_set(&(afd_res->f), nom_d);
+					insert_set(&(afd_res->Q), nom_d);
+					tdata inter_f = intersection_set(conj_t,afnd->F);
+					if(length(inter_f)>0)
+						insert_set(&(afd_res->F), nom_d);
 					free_tdata(inter_f);
 					
 					append(&sub,conj_t);
@@ -479,15 +485,16 @@ automata convertir(automata afnd){
 					append(&sub_p, conj_t);
 					append(&nom_p, nom_d);
 				}
-				agregar_transi(afd_res, nom_a, sim, nom_d);
+				int f_res =indice_esta(afd_res, nom_a);
+				agregar_transi(afd_res, f_res, le, conj_t);
 			}
+			le++;
 			aux_sim = aux_sim->next;
 		}
 		tdata bor_sub = sub_p;
 		tdata bor_nom = nom_p;
 		sub_p = sub_p->next;
 		nom_p = nom_p->next;
-		
 		free(bor_sub);
 		free(bor_nom);
 	}
