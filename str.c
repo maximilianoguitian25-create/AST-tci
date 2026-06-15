9916928f7cea10b56be8185e4e8c0233db0d0eb6
maximilianoguitian25-create <maximilianoguitian25@gmail.com>
2026-06-15 20:39:44 -0300

    cambio 1506

--- str.c
+++ str.c
@@ -1,46 +1,45 @@
 #include "str.h"
 
-str load2(const char* s){
-	if(s == NULL || *s == '\0')
-		return NULL;
-	
-	str n = (str)malloc(sizeof(struct charNode));
-	n->letra = *s;
-	n->next = load2(s+1);
+str load2(const char* s) {
+    if (s == NULL || *s == '\0')
+        return NULL;
 
-	return n;
-}
-	
-void print_str(str s){
-	str aux = s;
-	while(aux != NULL){
-		
-			printf("%c", aux->letra);
-		
-		aux = aux->next;
-	}
+    str n = (str)malloc(sizeof(struct charNode));
+    n->letra = *s;
+    n->next = load2(s + 1);
+
+    return n;
 }
 
-int igual_str(str s1, str s2){
-	if(s1 == NULL && s2 == NULL)
-		return 1;
-	if(s1 == NULL || s2 == NULL || s1->letra != s2->letra )
-		return 0;
-	
-	return igual_str(s1->next, s2->next);
-}
-str copy_str(str s){
-	if(s == NULL)
-		return NULL;
-	str n = (str)malloc(sizeof(struct charNode));
-	n->letra = s->letra;
-	n->next = copy_str(s->next);
-	return n;
+void print_str(str s) {
+    str aux = s;
+    while (aux != NULL) {
+        printf("%c", aux->letra);
+        aux = aux->next;
+    }
 }
 
-void free_str(str s){
-	if(s == NULL)
-		return;
-	free_str(s->next);
-	free(s);
+int igual_str(str s1, str s2) {
+    if (s1 == NULL && s2 == NULL)
+        return 1;
+    if (s1 == NULL || s2 == NULL || s1->letra != s2->letra)
+        return 0;
+
+    return igual_str(s1->next, s2->next);
+}
+
+str copy_str(str s) {
+    if (s == NULL)
+        return NULL;
+    str n = (str)malloc(sizeof(struct charNode));
+    n->letra = s->letra;
+    n->next = copy_str(s->next);
+    return n;
+}
+
+void free_str(str s) {
+    if (s == NULL)
+        return;
+    free_str(s->next);
+    free(s);
 }
