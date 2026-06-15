fae1848ff074f39d023c026d9af3a238836a4287
maximilianoguitian25-create <maximilianoguitian25@gmail.com>
2026-06-15 20:39:03 -0300

    cambios 15/06

--- str.h
+++ str.h
@@ -5,14 +5,15 @@
 #include <stdlib.h>
 
 struct charNode {
-	char letra;
-	struct charNode* next;
+    char letra;
+    struct charNode* next;
 };
 typedef struct charNode* str;
 
-str load2(const char*  );
-void print_str(str );
-int igual_str(str, str);
-str copy_str(str );
+str load2(const char* s);
+void print_str(str s);
+int igual_str(str s1, str s2);
+str copy_str(str s);
 void free_str(str s);
+
 #endif
