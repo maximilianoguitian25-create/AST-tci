6692b5830d3457f7b4b7ed567bd3f382f3168055
maximilianoguitian25-create <maximilianoguitian25@gmail.com>
2026-06-15 20:57:57 -0300

    Update main 2.c

--- main 2.c
+++ main 2.c
@@ -1,37 +1,81 @@
-#ifndef AFD_H
-#define AFD_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+#include "afd.h"
 
-#include "tdata.h"
-#include "str.h"
+int main() {
+    printf("--- Iniciando programa ---\n");
+    automata a = carga_aut(); 
+    
+    if (a == NULL) {
+        printf("Error: No se pudo cargar el automata. Saliendo...\n");
+        return 1;
+    }
+    
+    int opc;
+    char cad[100]; 
+    
+    do{
+        menu();
+        printf("Ingrese una opcion: ");
+        scanf("%d", &opc);
+        
+        switch(opc) {
+        case 1:
+            mostrar(a);
+            break;
+            
+        case 2:
+            if (es_afd(a)) printf("Es AFD\n");
+            else printf("Es AFND\n");
+            break;
+            
+        case 3:{
+            int b=1;
+            while(b==1) {
+                printf("\nIngrese la cadena a evaluar (o '0' para volver al menu): ");
+                leecad(cad,100);
+                getchar();
+                
+                if (strcmp(cad, "0") == 0) {
+                    b=0;
+                }
+                else{
+                    if (pertenece_cadena(a, cad)) {
+                        printf(">> ACEPTADA\n");
+                    } else {
+                        printf(">> RECHAZADA\n");
+                    }
+                }
+            }
+            break;
+        }
+        case 4:{
+            automata afd=convertir(a);
+            if(afd != NULL)
+                mostrar(afd);
+            else
+                printf("Ya es un afd");
+            }
+            break;
+        case 5:
+            liberar(a);
+            printf("PROFE APRUEBEME");
+            break;
+            
+        default:
+            printf("Opcion no valida.\n");
+        }
+    }while(opc!=5);
+        
+    return 0;
+}
 
-typedef struct {
-    int from;
-    int symbol;
-    tdata to;
-} TransitionEntry;
-
-typedef struct {
-    tdata Q;
-    tdata Sigma;
-    TransitionEntry* delta;
-    int deltaSize;
-    int q0;
-    tdata F;
-} Automata;
-
-typedef Automata* automata; 
-
-automata crea_uno(tdata conj_q, tdata alfa_sig, int ini, tdata conj_f);
-void agregar_transi(automata a, int from, int symbol, tdata est_dest);
-void mostrar(automata a);
-int pertene_cadena(automata a, tdata cad_list); 
-int es_afd(automata a);
-void liberar(automata a);
-int indice_esta(automata a, tdata estado);
-int indice_simb(automata a, tdata simbolo);
-automata carga_aut();
-int vali_cad(automata a, tdata cad);
-automata convertir(automata afnd);
-tdata leer_conjunto(char texto[]);
-
-#endif
+void menu(){
+    printf("\n--- MENU ---\n");
+    printf("1. Mostrar tabla de transiciones\n");
+    printf("2. Verificar si es AFD o AFND\n");
+    printf("3. Verificar si una cadena pertenece al automata\n");
+    printf("4. Convertir AFND a AFD\n");
+    printf("5. Salir\n");
+}
